3566b41bde1d8629e2526e35c4343eb65aaceb63
Spencer Sharpe <jspencersharpe@gmail.com>
2014-12-01 15:56:34 -0600

    new js file, alerting incorrectly

--- js/main.js
+++ js/main.js
@@ -2,14 +2,18 @@
   "use strict";
   var $xOro;
   var counter = 0;
-  $('.space').click(function(e) {
+  $('.space').one('click', function(e) {
     counter++;
     if (counter % 2 === 0) {
       $xOro = "O";
+      //$(this).removeClass(".0");
+      //$(this).addClass(".0");
     } else {
       $xOro = "X";
+      //$(this).removeClass(".X")
+      //$(this).addClass(".X");
     }
-      e.stopPropagation();
+       e.stopPropagation();
       $(this).append($xOro);
     });
 }());
