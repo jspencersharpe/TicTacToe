7f2522c4c24f7886da0919e17bc80e84d3b54c7c
Spencer Sharpe <jspencersharpe@gmail.com>
2015-05-29 10:35:01 -0500

    installed Grunt and Babel

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 currentMove = 'X';
-var moveCount = 9;
+var movesLeft = 9;
 
 $('td').on('click', function() {
   var $this = $(this);
@@ -8,11 +8,29 @@ $('td').on('click', function() {
     $(this).text(currentMove);
     currentMove = currentMove === 'X' ? 'O' : 'X';
 
-    if (movesLeft--) {
+    if (--movesLeft) {
       $('.status').text("${currentMove}'s Move");
     } else {
       $('.status').text("It's a tie");
     }
-    
+    whoWon();
   }
 });
+
+function whoWon() {
+  var td1 = $('td:nth-child(1)').text();
+  var td2 = $('td')[1].text();
+  var td3 = $('td')[2].text();
+  var td4 = $('td')[3].text();
+  var td5 = $('td')[4].text();
+  var td6 = $('td')[5].text();
+  var td7 = $('td')[6].text();
+  var td8 = $('td')[7].text();
+  var td9 = $('td')[8].text();
+  
+  if (td1 && td1 === td2 === td3) {
+    $('.status').text(`${td1} Wins`);
+  }
+  
+  
+}
